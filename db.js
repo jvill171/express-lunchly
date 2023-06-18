e3a6be6f9d102e046a32fe75c9a9b0dcffd8808f
jvill171 <juanvillarreal64@yahoo.com>
2023-06-18 18:41:11 -0500

    Corrected code to work with WSL: Ubuntu

--- db.js
+++ db.js
@@ -2,7 +2,9 @@
 
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client({
+    host: "/var/run/postgresql/",
+    database: "lunchly"});
 
 db.connect();
 
